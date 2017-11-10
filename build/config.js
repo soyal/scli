3f3664201c777f36e84c40477a6915c8aaadb618
soyal <xiongxiao9958@gmail.com>
2017-11-10 11:54:35 +0800

    untrack build files

--- build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    supportProjects: ['component', 'project-pc'],
-    compiledSuffixes: ['md', 'js', 'jsx', 'json', 'html'] // 需要解析的模板后缀
-};
-//# sourceMappingURL=config.js.map

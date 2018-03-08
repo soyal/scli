edc367922f9e9ff5bcfe5a89596520802428da00
Soyal <345723940@qq.com>
2018-03-08 15:25:08 +0800

    V1.1.0 (#3)
    * v1.1.0:
    新增对mobx模板的支持

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export default {
-  supportProjects: ['component', 'project-pc', 'eslint'],  // 支持的项目名
-  compiledSuffixes: ['md', 'js', 'json', 'html']  // 需要解析的模板后缀
+  supportProjects: ['component', 'project-pc', 'eslint', 'mobx', 'mobx-mobile'], // 支持的项目名
+  compiledSuffixes: ['md', 'js', 'json', 'html'] // 需要解析的模板后缀
 }

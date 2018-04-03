c7a5ab24eee2f2288476d2558f6fee499105230a
Soyal <345723940@qq.com>
2018-04-03 21:41:13 +0800

    support parcel template

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export default {
-  supportProjects: ['component', 'project-pc', 'eslint', 'mobx', 'mobx-mobile'], // 支持的项目名
+  supportProjects: ['component', 'project-pc', 'eslint', 'mobx', 'mobx-mobile', 'parcel'], // 支持的项目名
   compiledSuffixes: ['md', 'js', 'json', 'html'] // 需要解析的模板后缀
 }
